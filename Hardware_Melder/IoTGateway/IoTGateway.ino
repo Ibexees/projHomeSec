46578a23b92264840871e7003465c20aae09d070
Marius <marius.marik.19@gmail.com>
2026-04-17 16:53:10 +0200

    feat: doorunit sends data via esp-now feat: doorunit finds correct channel to send data feat: gateway recieves esp-now data feat: gateway can send mqtt messages to server

---
+++ Hardware_Melder/IoTGateway/IoTGateway.ino
@@ -0,0 +1,195 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <esp_now.h>
+#include "ESP32MQTTClient.h"
+#include "esp_idf_version.h" // check IDF version
+#include <credentials.h>
+#include "esp_wifi.h"
+
+const char *ssid = WIFI_SSID;
+const char *pass = WIFI_PASSWORD;
+
+char *mqttServer = MQTT_BROKER_ADRESS ;
+char *mqttUser = MQTT_USER;
+char *mqttPassword = MQTT_PASSWORD;
+
+char *subscribeTopic = "foo";
+char *publishTopic = "bar/bar";
+
+ESP32MQTTClient mqttClient;
+
+int pubCount = 0;
+
+volatile bool newEspNowMessage = false;
+
+
+typedef struct sensor_message {
+  char sensorId[16];
+  bool isOpen;
+  int battery;
+  int bootCount;
+} sensor_message;
+
+
+sensor_message incomingMessage;
+
+void onEspNowDataRecv(const esp_now_recv_info_t *info, const uint8_t *data, int len) {
+  char macStr[18];
+
+  snprintf(macStr, sizeof(macStr),
+           "%02X:%02X:%02X:%02X:%02X:%02X",
+           info->src_addr[0],
+           info->src_addr[1],
+           info->src_addr[2],
+           info->src_addr[3],
+           info->src_addr[4],
+           info->src_addr[5]);
+
+  Serial.println("-----------------------------------");
+  Serial.print("Message received from: ");
+  Serial.println(macStr);
+
+  if (len != sizeof(sensor_message)) {
+    Serial.println("Invalid message size");
+    return;
+  }
+
+  memcpy(&incomingMessage, data, sizeof(incomingMessage));
+
+  Serial.print("Sensor ID: ");
+  Serial.println(incomingMessage.sensorId);
+
+  Serial.print("Door/Window Open: ");
+  Serial.println(incomingMessage.isOpen ? "YES" : "NO");
+
+  Serial.print("Battery: ");
+  Serial.print(incomingMessage.battery);
+  Serial.println(" %");
+
+  Serial.print("Boot Count: ");
+  Serial.println(incomingMessage.bootCount);
+
+  Serial.println("ALARM: Door or window opened!");
+
+
+  
+  
+  newEspNowMessage = true;
+    // Hier könntest du später ergänzen:
+    // - Sirene aktivieren
+    // - HTTP Request ans Backend senden
+    // - MQTT Nachricht senden
+    // - Datenbankeintrag erzeugen
+
+  Serial.println("-----------------------------------");
+}
+
+void printMacAddress() {
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+
+  Serial.println();
+  Serial.println("Gateway MAC Address:");
+  Serial.printf("{0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X, 0x%02X}\n",
+                mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+  Serial.println();
+}
+
+void mqttSetup()
+{
+      // Serial.begin(115200);
+    log_i();
+    log_i("setup, ESP.getSdkVersion(): ");
+    log_i("%s", ESP.getSdkVersion());
+
+    mqttClient.enableDebuggingMessages();
+
+    mqttClient.setURI(mqttServer, mqttUser, mqttPassword);
+    mqttClient.enableLastWillMessage("lwt", "I am going offline");
+    mqttClient.setKeepAlive(30);
+    mqttClient.setOnMessageCallback([](const std::string &topic, const std::string &payload) {
+        log_i("Global callback: %s: %s", topic.c_str(), payload.c_str());
+    });
+    WiFi.begin(ssid, pass);
+
+    while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+}
+    WiFi.setSleep(false);
+    esp_wifi_set_ps(WIFI_PS_NONE);
+    esp_wifi_set_channel(WiFi.channel(), WIFI_SECOND_CHAN_NONE);
+
+    Serial.print("WiFi status: ");
+    Serial.println(WiFi.status());
+
+    Serial.print("ESP IP: ");
+    Serial.println(WiFi.localIP());
+
+    mqttClient.loopStart();
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  
+
+  printMacAddress();
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("ESP-NOW init failed");
+    return;
+  }
+
+  esp_now_register_recv_cb(onEspNowDataRecv);
+
+  Serial.println("ESP-NOW Gateway Ready");
+
+  mqttSetup();
+}
+
+uint8_t primaryChannel;
+wifi_second_chan_t secondChannel;
+
+void loop() {
+  if(newEspNowMessage)
+  {
+    std::string msg = "Hello: " + std::to_string(pubCount++);
+    mqttClient.publish(publishTopic, msg, 0, false);
+    newEspNowMessage = false;
+  }
+  esp_wifi_get_channel(&primaryChannel, &secondChannel);
+
+  Serial.print("Current WiFi channel: ");
+  Serial.println(primaryChannel);
+  delay(1000);
+}
+
+void onMqttConnect(esp_mqtt_client_handle_t client)
+{
+    if (mqttClient.isMyTurn(client)) // can be omitted if only one client
+    {
+        mqttClient.subscribe(subscribeTopic, [](const std::string &payload)
+                             { log_i("%s: %s", subscribeTopic, payload.c_str()); });
+
+        mqttClient.subscribe("bar/#", [](const std::string &topic, const std::string &payload)
+                             { log_i("%s: %s", topic.c_str(), payload.c_str()); });
+    }
+}
+
+#if ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(5, 0, 0)
+esp_err_t handleMQTT(esp_mqtt_event_handle_t event)
+{
+    mqttClient.onEventCallback(event);
+    return ESP_OK;
+}
+#else  // IDF CHECK
+void handleMQTT(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    auto *event = static_cast<esp_mqtt_event_handle_t>(event_data);
+    mqttClient.onEventCallback(event);
+}
+#endif // // IDF CHECK
--- Hardware_Melder/SleepWakeupTest/SleepWakeupTest.ino
+++ Hardware_Melder/SleepWakeupTest/SleepWakeupTest.ino
@@ -1,8 +1,74 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <esp_now.h>
+#include <esp_sleep.h>
+#include "esp_wifi.h"
+
+#define BAUD 115200
 #define BUTTON_PIN_BITMASK 0x200000000 // 2^33 in hex
 
 const int led = D10;
 const int wakePin = D1;
 RTC_DATA_ATTR int bootCount = 0;
+RTC_DATA_ATTR int lastWorkingChannel = 3;
+
+//ESP-NOW setup
+typedef struct sensor_message {
+  char sensorId[16];
+  bool isOpen;
+  int battery;
+  int bootCount;
+} sensor_message;
+
+uint8_t gatewayMac[] = {0x80, 0xB5, 0x4E, 0xF3, 0xB1, 0xC4};
+
+esp_now_peer_info_t peerInfo;
+sensor_message msg;
+volatile bool sendFinished = false;
+volatile bool sendSuccess = false;
+
+int startChannelScan()
+{
+  for (int ch = 1; ch <= 13; ch++) {
+  esp_wifi_set_channel(ch, WIFI_SECOND_CHAN_NONE);
+  Serial.print("Trying:");
+  Serial.println(ch);
+
+  sendFinished = false;
+
+  esp_now_send(gatewayMac, (uint8_t*)&msg, sizeof(msg));
+
+  unsigned long start = millis();
+  while (!sendFinished && millis() - start < 500) {
+    delay(10);
+  }
+
+  if (sendSuccess) {
+    Serial.print("Found working channel: ");
+    Serial.println(ch);
+    return ch;
+  }
+  
+
+}
+return -1;
+}
+
+void onDataSent(const wifi_tx_info_t *tx_info, esp_now_send_status_t status) {
+  Serial.print("Send status: ");
+
+  if(status == ESP_NOW_SEND_SUCCESS)
+  {
+    Serial.println("Success");
+    sendSuccess = true;
+  }
+  else
+  {
+    sendSuccess = false;
+  }
+
+  sendFinished = true;
+}
 
 /*
 Method to print the reason by which ESP32
@@ -24,11 +90,12 @@ void print_wakeup_reason(){
   }
 }
 
-void setup(){
+void sleepmodeSetup()
+{
   pinMode(led, OUTPUT);
   pinMode(wakePin, INPUT);
 
-  Serial.begin(115200);
+  Serial.begin(BAUD);
   delay(1000); //Take some time to open up the Serial Monitor
 
   //Increment boot number and print it every reboot
@@ -39,24 +106,115 @@ void setup(){
   print_wakeup_reason();
 
   esp_deep_sleep_enable_gpio_wakeup(BIT(D1), ESP_GPIO_WAKEUP_GPIO_HIGH);
+}
 
-  
+void espNowSetup()
+{
+   WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    WiFi.setSleep(false);
+    esp_wifi_set_channel(lastWorkingChannel, WIFI_SECOND_CHAN_NONE);
+
+
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("ESP-NOW init failed");
+    return;
+  }
+
+  esp_now_register_send_cb(onDataSent);
+
+  memcpy(peerInfo.peer_addr, gatewayMac, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  Serial.println("ESP-NOW ready");
+}
+
+void sendSensorState(bool isOpen) {
+  strcpy(msg.sensorId, "window_kitchen");
+  msg.isOpen = isOpen;
+  msg.battery = 92;
+  msg.bootCount = bootCount;
+
+  esp_wifi_set_channel(lastWorkingChannel, WIFI_SECOND_CHAN_NONE);
+
+  sendFinished = false;
+  sendSuccess = false;
+
+  esp_err_t result = esp_now_send(
+    gatewayMac,
+    (uint8_t *) &msg,
+    sizeof(msg)
+  );
+
+  if (result == ESP_OK) {
+    Serial.println("Message queued");
+  } else {
+    Serial.println("Send error");
+  }
+
+  unsigned long start = millis();
+  while (!sendFinished && millis() - start < 2000) {
+    delay(10);
+  }
+
+   if (!sendSuccess) {
+    Serial.println("Send failed, starting channel scan");
+
+    int foundChannel = startChannelScan();
+
+    if (foundChannel != -1) {
+      lastWorkingChannel = foundChannel;
+
+      Serial.print("Saved working channel: ");
+      Serial.println(lastWorkingChannel);
+    } else {
+      Serial.println("No working channel found");
+    }
+  }
+}
+
+void setup(){
+
+  sleepmodeSetup();
+  espNowSetup();
   
   
 }
+
+uint8_t primaryChannel;
+wifi_second_chan_t secondChannel;
+
+
 
 void loop(){
 
   if(digitalRead(wakePin) == LOW)
   {
-  Serial.println("Going to sleep now");
-  esp_deep_sleep_start();
+    sendSensorState(true);
+    Serial.println("Going to sleep now");
+    esp_deep_sleep_start();
   }
-  Serial.println("Skipped Sleep");
+  
+  esp_wifi_get_channel(&primaryChannel, &secondChannel);
+
+  Serial.print("Current WiFi channel: ");
+  Serial.println(primaryChannel);
+  
+  //send Data to iot Gateway
+  sendSensorState(true);
+  //esp-now connection
 
   digitalWrite(led, HIGH);   // turn the LED on 
-  delay(1000);               // wait for a second
+  delay(10000);               // wait for a second
   digitalWrite(led, LOW);    // turn the LED off
-  delay(1000);               // wait for a second
+  delay(10000);               // wait for a second
  
 }
+
+
